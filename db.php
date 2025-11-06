06ff0dce98ba743c52cdbddae73def180943a066
pleyita23 <juradom370@gmail.com>
2025-11-06 08:43:39 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "inventario_db");
+$conexion = new mysqli("mysql-flaw.alwaysdata.net", "flaw", "Misifu123+", "flaw_inventario");
 if ($conexion->connect_error) {
   die("Error de conexión: " . $conexion->connect_error);
 }
