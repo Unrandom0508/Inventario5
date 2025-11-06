63af751769022d05f66f87533c514b5749838262
pleyita23 <juradom370@gmail.com>
2025-11-06 09:12:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
-  <h2>Iniciar Sesión</h2>
+  <h2> INICIAR SESIÓN </h2>
   <form action="login.php" method="POST">
     <label>Cédula:</label>
     <input type="text" name="cedula" required><br>
